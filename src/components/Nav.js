e74806a9c996d750d375d23cf10505825dad8c0a
github username <github email address>
2021-04-05 15:13:22 -0700

    added links to home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Nav extends Component {
     render() {
         return (
             <div>
-                Nav
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
             </div>
         )
     }
