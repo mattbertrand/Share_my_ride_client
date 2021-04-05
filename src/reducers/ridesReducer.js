d8011c7cf531037f4829e4590ea3c778e48c11e6
github username <github email address>
2021-04-05 14:40:08 -0700

    added a rides Reducer

---
+++ src/reducers/ridesReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    rides: [],
+    loading: true
+}
+
+const ridesReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
+export default ridesReducer
