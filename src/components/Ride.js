fbbc27763141ef3f04221a5f6eeede10e3b14b36
github username <github email address>
2021-04-05 14:53:30 -0700

    added a ride component

---
+++ src/components/Ride.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Ride extends Component {
+    render() {
+        return (
+            <div>
+                Ride
+            </div>
+        )
+    }
+}
+
+export default Ride
